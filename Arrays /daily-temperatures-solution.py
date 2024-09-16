e36c034f2f869aea3b7323e6061455f31acba1e5
Tristanleif1 <tristanallaman@gmail.com>
2024-09-15 19:28:47 -0700

    Added better solution to daily temperatures problem

--- Arrays /daily-temperatures-solution.py
+++ Arrays /daily-temperatures-solution.py
@@ -9,3 +9,21 @@ class Solution:
                     ans[i] = j - i
                     break
         return ans
+
+
+# Monotonic decreasing stack #O(N) Time Complexity
+
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        #Monotonic decreasing stack
+        numOfDays = [0] * len(temperatures)
+        stack = []
+
+        for i, temp in enumerate(temperatures):
+            while stack and temperatures[stack[-1]] < temp:
+                prevI = stack.pop()
+                numOfDays[prevI] = i - prevI
+
+            stack.append(i)
+
+        return numOfDays
